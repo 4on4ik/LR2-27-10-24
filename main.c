a4490d4356df57fb96a028777add73e7cc951b71
4on4ik <dredfile4@gmail.com>
2024-10-28 17:58:17 +0300

    Main.c

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
-int main() {
-    printf("vasya\n");
-    printf("Hello world!");
+
+int main(void) {
+    int n = 3;
+    float E;
+    float num_1;
+    float num_2;
+    int i = 1;
+    float sum_1;
+
+    num_1 = (float)5 / (3 * (n+2) * (n+2) +1);
+    // Вводим num_1 и num_2
+
+    printf("write error(write less 1 please):\n");
+    scanf("%f", &E);
+    printf("%f\n", E);
+    // Вводим погрешность
+
+    sum_1 = num_1;
+    printf("%f\n", sum_1);
+    //Вводим переменный отвечающие за сумму рядов текущей(sum_1) и предыдущей итерации(sum_2)
+
+    int z = 0;
+
+    while(z == 0){
+        float sum_2;
+        i += 1 ;
+        n += 1;
+        num_1 = (float)5 / (3 * (n+2) * (n+2) +1);
+        sum_2 = sum_1;
+        sum_1 += num_1;
+
+
+        if ((sum_1 - sum_2) < E){
+            z=1;
+            
+        }
+    }
+    printf("i, for which the sum of the infinitely convergent series will be less than the given E =%d\n", i);
     return 0;
 }
+
